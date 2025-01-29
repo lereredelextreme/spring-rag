373f971008e8ce5dcf86955e167b1b7580d22024
lereredelextreme <aurelien.dasse19@gmail.com>
2025-01-29 10:03:21 +0100

    gradle

--- src/main/java/fr/efrei/rag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController
 public class SampleResource {
 
     @GetMapping("/samples/{value}")
     public String hello(@PathVariable(value = "value", required = false) final String value) {
-        return "Hello" + value + "!";
+        return "Hello " + value + "!";
     }
 
     @GetMapping("/samples/dto/{value}")
     public Sample helloJson(@PathVariable(value = "value", required = false) final String value) {
-        return new Sample("Hello" + value + "!");
+        return new Sample("Hello " + value + "!");
     }
 }
